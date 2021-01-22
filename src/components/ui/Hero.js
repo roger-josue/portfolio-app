79db6f06112e6cec6b8c51f32af1bb67ced1e419
Josue <jovz1798@gmail.com>
2021-01-22 17:06:51 -0600

    typewriter effect on hero title and gradient animation

--- src/components/ui/Hero.js
+++ src/components/ui/Hero.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { scroller } from 'react-scroll';
 import { Navbar } from './Navbar';
 export const Hero = () => {
+
+    const typewriter = useRef(null);
+
+    useEffect(() => {
+        
+        createTypewriter();
+        typewriter.current.style = 'animation: gradient-in 6s 0s 5 alternate forwards';
+
+    },[]);
+
+    const createTypewriter = async() => {
+        const text = 'Hello, I’m Josue Vargas. I’m a front-end developer';
+        
+        for (const char of text) {
+            await new Promise( ( resolve )=>{    
+                setTimeout(() => {
+                    resolve();
+                }, 100);
+            })
+            typewriter.current.innerHTML += char;
+        }
+
+        // TODO: type end bar: | blink effect
+        // typewriter.current.innerHTML += '|';
+
+    }
 
     const handleCallToAction = () => {
         scroller.scrollTo('projects-section');
@@ -11,8 +37,7 @@ export const Hero = () => {
         <header>
             <Navbar />
             <div id="hero">
-                <h1>Hello, I’m <strong>Josue Vargas.</strong>
-                    I’m a front-end developer </h1>
+                <h1 ref={ typewriter } > </h1>
                 <button 
                     id="to-work"
                     onClick={ handleCallToAction }>View my work <i className="fas fa-arrow-right"></i> </button>
