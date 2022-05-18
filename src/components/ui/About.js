b2475e9bd9429d6710f96e3c489391fa89571f85
Josue Vargas Zúñiga <jovz1798@gmail.com>
2022-05-18 16:08:23 +0000

    About component profiles mods

--- src/components/ui/About.js
+++ src/components/ui/About.js
@@ -7,10 +7,10 @@ import { Skills } from './Skills';
 
 export const About = () => {
 
-    const { ref, inView } = useInView({threshold: 0.5, triggerOnce: true});
+    const { ref, inView } = useInView({ threshold: 0.5, triggerOnce: true });
 
     return (
-        <section id="about-section" ref={ ref }>
+        <section id="about-section" ref={ref}>
             <h1 className="section-title">About</h1>
             {
                 (inView) &&
@@ -18,22 +18,15 @@ export const About = () => {
                     <article className="about-col profile">
                         <h2>Who am I?</h2>
                         <figure>
-                            <img src={ profileImg } alt="profile" />
+                            <img src={profileImg} alt="profile" />
                         </figure>
-                        <p>I’m a Systems Engineer graduated at Universidad Internacional San Isidro Labrador in February 2020.
-                            I’ve always had a passion for building things, and because of that feeling, I became a front-end
-                            developer.
-                            Now I have a serious passion for creating dynamic and responsive web UI.
-                            Currently, I'm focusing on React, 
-                            but I'm always open to learn new technologies and seize new opportunities.
-                            <br />
-                        </p>
+                        <p>Creative Systems Engineer and Software developer based in Costa Rica, with a passion for building responsive websites and pixel-perfect UI’s, proficient and self-sufficient in JavaScript, HTML, CSS, SASS, React, Git, GitHub, among other tools. Currently, becoming a full-stack engineer and sharpening my front-end skills. I’m always open to learn new technologies and seize new opportunities. <br /></p>
                         <div className="shakeText">
                             <Link className="link" to="contact-section">Let's talk!</Link>
                         </div>
                     </article>
                     <Skills />
-                </>              
+                </>
             }
         </section>
     )
