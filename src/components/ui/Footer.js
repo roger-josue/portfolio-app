f8564810dd08bddccb888f144b6de833fbe04867
josue <jovz1798@gmail.com>
2022-08-27 13:36:14 -0600

    Deleted some spaces in the footer.js component

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -7,10 +7,10 @@ export const Footer = () => {
         <footer>
         <nav>
             <ul id="social-menu">
-                <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/roger-josue-vargas-z-260681190/"> <i className="fab fa-linkedin-in"></i> </a></li>
-                <li><a target="_blank" rel="noreferrer" href="https://www.facebook.com/josue.vargaszuniga.1"> <i className="fab fa-facebook-f"></i> </a></li>
-                <li><a target="_blank" rel="noreferrer" href="https://www.instagram.com/josue_vargas_z/?fbclid=IwAR0lLIQm0XJeuuA_iPIY48iknvczGQVHmNSKC1OoVIoYDR5WIBgSKI8DwGw"> <i className="fab fa-instagram"></i> </a></li>
-                <li><a target="_blank" rel="noreferrer" href="https://github.com/JosueVargasZ"> <i className="fab fa-github"></i> </a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/roger-josue-vargas-z-260681190/"> <i className="fab fa-linkedin-in"></i></a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://www.facebook.com/josue.vargaszuniga.1"> <i className="fab fa-facebook-f"></i></a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://www.instagram.com/josue_vargas_z/?fbclid=IwAR0lLIQm0XJeuuA_iPIY48iknvczGQVHmNSKC1OoVIoYDR5WIBgSKI8DwGw"><i className="fab fa-instagram"></i></a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://github.com/JosueVargasZ"><i className="fab fa-github"></i></a></li>
             </ul>
         </nav>
         <Link to="hero" id="arrow-home"> <i className="fas fa-chevron-up"></i> </Link>
